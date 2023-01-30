1fd112fb86ec9a4f152f1d65ff4d5496883f7d6c
_devOck <respecttowardyou@gmail.com>
2023-01-30 22:39:37 +0900

    Init RoomUserHistory codes

---
+++ src/main/java/kr/onthelive/training/repository/mapper/RoomUserHistoryMapper.java
@@ -0,0 +1,12 @@
+package kr.onthelive.training.repository.mapper;
+
+import kr.onthelive.training.model.BaseRoomUserHistory;
+
+public interface RoomUserHistoryMapper {
+    // select
+    BaseRoomUserHistory selectRoomUserHistory(String id);
+    // insert
+    int insertRoomUserHistory(BaseRoomUserHistory roomUserHistory);
+
+}
+
---
+++ src/main/java/kr/onthelive/training/service/RoomUserHistoryService.java
@@ -0,0 +1,10 @@
+package kr.onthelive.training.service;
+
+import kr.onthelive.training.repository.RoomUserHistoryRepository;
+
+public class RoomUserHistoryService {
+    private RoomUserHistoryRepository roomUserHistoryRepository;
+
+// 미정
+
+}
