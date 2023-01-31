e6456c7ef0ecad926a5991ee0111e1e1daa34461
_devOck <respecttowardyou@gmail.com>
2023-01-31 14:41:45 +0900

    Update RoomUserHistory codes

--- src/main/java/kr/onthelive/training/repository/RoomUserHistoryRepository.java
+++ src/main/java/kr/onthelive/training/repository/RoomUserHistoryRepository.java
@@ -2,12 +2,14 @@ package kr.onthelive.training.repository;
 
 import kr.onthelive.training.model.BaseRoomUserHistory;
 import kr.onthelive.training.repository.mapper.RoomUserHistoryMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@Slf4j
 public class RoomUserHistoryRepository {
     private RoomUserHistoryMapper mapper;
 
@@ -18,11 +20,13 @@ public class RoomUserHistoryRepository {
 
     // select
     public BaseRoomUserHistory selectRoomUserHistory(String id) {
+        log.trace("repository.selectRoomUserHistory... {}", id);
         return mapper.selectRoomUserHistory(id);
     }
 
     // insert
     public int insertRoomUserHistory(BaseRoomUserHistory roomUserHistory) {
+        log.trace("repository.insertRoomUserHistory... {}", roomUserHistory);
         return mapper.insertRoomUserHistory(roomUserHistory);
     }
 
--- src/main/java/kr/onthelive/training/service/RoomUserHistoryService.java
+++ src/main/java/kr/onthelive/training/service/RoomUserHistoryService.java
@@ -1,14 +1,27 @@
 package kr.onthelive.training.service;
 
+import kr.onthelive.training.model.BaseRoomUserHistory;
 import kr.onthelive.training.repository.RoomUserHistoryRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class RoomUserHistoryService {
-    private RoomUserHistoryRepository roomUserHistoryRepository;
+    private RoomUserHistoryRepository repository;
 
-// 미정
+    @Autowired
+    public RoomUserHistoryService(RoomUserHistoryRepository repository){ this.repository = repository;}
+
+    // id로 조회(test)
+    public BaseRoomUserHistory getRoomUserHistory(String id){
+        log.trace("service.getRoomUserHistory start... {}", id);
+        return repository.selectRoomUserHistory(id);}
+
+    // 입력(test)
+    public int createRoomUserHistory(BaseRoomUserHistory roomUserHistory){
+        return repository.insertRoomUserHistory(roomUserHistory);
+    }
 
 }
